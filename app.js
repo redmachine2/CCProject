02d21d4f57ee4fa55d8aa6ba0992da5fb07717ce
Jason <jlynn911@gmail.com>
2015-03-04 22:15:37 -0600

    changed port to 80

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/www));
+app.use(express.static(__dirname + '/www'));
 
-app.listen(8080);
+app.listen(80);
